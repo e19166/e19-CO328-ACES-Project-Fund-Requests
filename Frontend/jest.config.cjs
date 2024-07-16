480ea6fdbaea86668953c1e3fac6bfb64e6ecd69
Wishula Jayathunga <e19166@eng.pdn.ac.lk>
2024-07-16 18:15:22 +0530

    Update jest.config.cjs

--- Frontend/jest.config.cjs
+++ Frontend/jest.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   //setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
