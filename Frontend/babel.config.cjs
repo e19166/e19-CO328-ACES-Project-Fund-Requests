4a50460ac436261eb97b5684da1d7970c0c58993
Wishula Jayathunga <e19166@eng.pdn.ac.lk>
2024-07-16 18:23:57 +0530

    Delete Frontend/babel.config.cjs

--- Frontend/babel.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }], // Targets the current Node.js version
-    '@babel/preset-react', // Enables JSX support
-    '@babel/preset-typescript', // Enables TypeScript support
-  ],
-};
